c55acc26ddb689f01bbd858e84aea3ce78f08d9c
shenghanqin <shenghanqin@hujiang.com>
2020-01-29 20:39:56 +0800

    update 更换脸部识别库

--- client/src/api/face.js
+++ client/src/api/face.js
@@ -1,22 +1,49 @@
 import * as faceapi from 'face-api.js';
 
+const SSD_MOBILENETV1 = 'ssd_mobilenetv1';
+const TINY_FACE_DETECTOR = 'tiny_face_detector';
+
+let selectedFaceDetector = SSD_MOBILENETV1;
+
+// ssd_mobilenetv1 options
+let minConfidence = 0.5;
+
+// tiny_face_detector options
+let inputSize = 512;
+let scoreThreshold = 0.5;
+
+
 const maxDescriptorDistance = 0.5;
+
+function getFaceDetectorOptions() {
+  return selectedFaceDetector === SSD_MOBILENETV1
+    ? new faceapi.SsdMobilenetv1Options({ minConfidence })
+    : new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold });
+}
+
+function getCurrentFaceDetectionNet() {
+  if (selectedFaceDetector === SSD_MOBILENETV1) {
+    return faceapi.nets.ssdMobilenetv1;
+  }
+  if (selectedFaceDetector === TINY_FACE_DETECTOR) {
+    return faceapi.nets.tinyFaceDetector;
+  }
+}
+
+
+
 
 export async function loadModels() {
   const MODEL_URL = process.env.PUBLIC_URL + '/models';
-  await faceapi.loadTinyFaceDetectorModel(MODEL_URL);
-  await faceapi.loadFaceLandmarkTinyModel(MODEL_URL);
+  // await faceapi.loadTinyFaceDetectorModel(MODEL_URL);
+  // await faceapi.loadFaceLandmarkTinyModel(MODEL_URL);
   await faceapi.loadFaceRecognitionModel(MODEL_URL);
+  await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
+  await faceapi.loadFaceLandmarkModel(MODEL_URL)
 }
 
 export async function getFullFaceDescription(blob, inputSize = 512) {
-  // tiny_face_detector options
-  let scoreThreshold = 0.5;
-  const OPTION = new faceapi.TinyFaceDetectorOptions({
-    inputSize,
-    scoreThreshold
-  });
-  const useTinyModel = true;
+  const OPTION = getFaceDetectorOptions()
 
   // fetch image to api
   let img = await faceapi.fetchImage(blob);
@@ -25,7 +52,7 @@ export async function getFullFaceDescription(blob, inputSize = 512) {
   // including landmark and descriptor of each face
   let fullDesc = await faceapi
     .detectAllFaces(img, OPTION)
-    .withFaceLandmarks(useTinyModel)
+    .withFaceLandmarks()
     .withFaceDescriptors();
   // const resizedResults = await faceapi.resizeResults(fullDesc, img);
   return fullDesc;
