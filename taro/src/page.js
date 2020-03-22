92b64b2bd40b47af04819a87b2b2aed363ecf7b4
shenghanqin <shenghanqin@163.com>
2020-03-22 21:44:58 +0800

    update 删除无用代码

--- taro/src/page.js
+++ taro/src/page.js
@@ -1,5 +1,5 @@
 import { connect } from '@tarojs/redux'
-import { modelName as userModel, getPassSdk } from '@/store/user'
+import { modelName as userModel } from '@/store/user'
 import { LOGIN_STATUS } from 'constants/status'
 import EventEmitter from 'utils/event-emitter'
 
@@ -58,55 +58,6 @@ export default function WithPage(TaroComponent) {
         sourceType: 'async',
         loginStatus
       })
-    }
-
-    // TODO 这个方法可以等待废弃
-    // componentWillReceiveProps(nextProps) {
-    //   console.log('nextProps.isLogin !== this.props.isLogin', nextProps.isLogin,this.props.isLogin)
-    //   if (nextProps.isLogin !== this.props.isLogin) {
-    //     // 当前页面登录无需回调该方法，只走页面页面自定义登录回调
-    //     // TODO: loginInfo.pageName !== this.$name 啥意思？？
-    //     debugger
-    //     if (typeof this.onInitLoginCallback === 'function') {
-    //       this.onInitLoginCallback({
-    //         ...nextProps.userInfo,
-    //         sourceType: 'async'
-    //       })
-    //     }
-    //   }
-    // }
-
-    // TODO 这个方法可以等待废弃
-    // componentDidUpdate (prevProps) {
-    //   console.log('prevProps.isLogin !== this.props.isLogin', prevProps.isLogin, this.props.isLogin)
-    //   if (prevProps.isLogin !== this.props.isLogin) {
-    //     // 当前页面登录无需回调该方法，只走页面页面自定义登录回调
-    //     // TODO: loginInfo.pageName !== this.$name 啥意思？？
-    //     debugger
-    //     if (typeof this.onInitLoginCallback === 'function') {
-    //       this.onInitLoginCallback({
-    //         ...this.props.userInfo,
-    //         sourceType: 'async'
-    //       })
-    //     }
-    //   }
-    // }
-
-
-    // TODO 这个方法可以等待废弃
-    /**
-     * 登录态监听
-     */
-    __checkLogin = () => {
-      if (typeof this.onInitLoginCallback === 'function') {
-        const { isLogin, userInfo } = this.props
-        const passSdk = getPassSdk() || {}
-        if (isLogin) {
-          this.onInitLoginCallback({ sourceType: 'sync', ...userInfo })
-        } else if (passSdk.initialized) {
-          this.onInitLoginCallback({ sourceType: 'autoLoginFail', ...userInfo })
-        }
-      }
     }
 
   }
