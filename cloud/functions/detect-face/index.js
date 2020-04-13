7300a1d0a0d5fa1c64ba6f511faad3ed279f3c42
shenghanqin <shenghanqin@163.com>
2020-04-13 22:27:20 +0800

    update 更新人脸识别函数方法

---
+++ cloud/functions/detect-face/index.js
@@ -0,0 +1,117 @@
+require('./promise-allSettled')
+const extCi = require("@cloudbase/extension-ci");
+const tcb = require('tcb-admin-node')
+const fetch = require('axios')
+const detectFace = require('./req-iai-face').detectFace
+
+let env = process.env.TCB_ENV === 'local' ? 'development-v9y2f' : process.env.TCB_ENV
+
+tcb.init({
+  env
+})
+tcb.registerExtension(extCi)
+
+
+const getBase64 = async (fileID) => {
+  let { fileContent } = await tcb.downloadFile({
+    fileID
+  })
+
+  return fileContent.toString('base64')
+}
+
+const getImageUrl = async (fileID) => {
+  const { fileList } = await tcb.getTempFileURL({
+    fileList: [fileID]
+  })
+  return fileList[0].tempFileURL
+}
+
+
+exports.main = async (event) => {
+  const { fileID = '', width = 600, height = 600 } = event
+
+  if (!fileID) {
+    let errorString = '请设置 fileID'
+    console.log(errorString)
+    return {
+      data: {},
+      time: new Date(),
+      status: -10086,
+      message: errorString
+    }
+  }
+
+  try {
+    let originImageUrl = await getImageUrl(fileID)
+    console.log('originImageUrl :', originImageUrl);
+  } catch (error) {
+    return {
+      data: {},
+      time: new Date(),
+      status: -10087,
+      message: JSON.stringify(error)
+    }
+  }
+  
+  // let imgID = fileID.replace('cloud://', '')
+  // let index = imgID.indexOf('/')
+  // let cloudPath = imgID.substr(index)
+  // let cloudEnvPath = imgID.substr(0, index)
+
+  // let facePath1 = '/face-recognition1'  
+  // const opts = {
+  //   rules: [
+  //     {
+  //       // 处理结果的文件路径，如以’/’开头，则存入指定文件夹中，否则，存入原图文件存储的同目录
+  //       fileid: facePath1 + cloudPath,
+  //       rule: "imageMogr2/scrop/800x800" // 处理样式参数，与下载时处理图像在url拼接的参数一致
+  //     }
+  //   ]
+  // }
+
+
+  // await tcb.invokeExtension("CloudInfinite", {
+  //   action: "ImageProcess",
+  //   cloudPath: cloudPath, // 存储图像的绝对路径，与tcb.uploadFile中一致
+  //   // fileContent, // 该字段可选，文件内容：Uint8Array|Buffer。有值，表示上传时处理图像；为空，则处理已经上传的图像
+  //   operations: opts
+  // });
+
+  // let faceFileID = cloudEnvPath + facePath1 + cloudPath
+
+  // // const base64 = await getBase64(faceFileID)
+  // const imageUrl = await getImageUrl(faceFileID)
+
+  // return Promise.allSettled([
+  //   tcb.callFunction({
+  //     name: 'image-safe-check',
+  //     data: {
+  //       fileID: faceFileID
+  //     }
+  //   }), 
+  //   detectFace(imageUrl)
+  // ]).then((results) => {
+  //   let checkResult = results[0]
+  //   let faceResult = results[1]
+
+  //   if (checkResult.result.status) {
+      
+  //     return checkResult.result
+  //   }
+
+  //   return {
+  //     ...faceResult,
+  //     data: {
+  //       ...faceResult.data,
+  //       faceFileID,
+  //       faceImageUrl: imageUrl
+  //     }
+  //   }
+  // }).catch(error => {
+  //   console.log('error :', error);
+
+  // })
+
+  
+}
