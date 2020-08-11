53a0a6bf8dba89a7fe0d4ab41e65df31fb81cbd1
shenghanqin <shenghanqin@163.com>
2020-08-11 21:46:07 +0800

    style: 删除无用的图片

--- taro/src/app.js
+++ taro/src/app.js
@@ -48,36 +48,12 @@ class App extends Component {
       color: '#95a1af',
       selectedColor: '#2f5aff',
       list: [
-        // {
-        //   pagePath: 'pages/image-watermark/image-watermark',
-        //   text: '盲水印添加',
-        //   iconPath: 'images/tab-bar-crown.png',
-        //   selectedIconPath: 'images/tab-bar-crown-active.png'
-        // },
         {
           pagePath: 'pages/avatar-edit/avatar-edit',
           text: '头像编辑',
           iconPath: 'images/tab-bar-crown.png',
           selectedIconPath: 'images/tab-bar-crown-active.png'
         },
-        // {
-        //   pagePath: 'pages/detect-face/detect-face',
-        //   text: '人像魅力',
-        //   iconPath: 'images/face-1.png',
-        //   selectedIconPath: 'images/face-1-active.png'
-        // },
-        // {
-        //   pagePath: 'pages/wear-a-mask/wear-a-mask',
-        //   text: '戴口罩',
-        //   iconPath: 'images/mask-1.png',
-        //   selectedIconPath: 'images/mask-2.png'
-        // },
-        // {
-        //   pagePath: 'pages/self/self',
-        //   text: '个人中心',
-        //   iconPath: 'images/thank-1.png',
-        //   selectedIconPath: 'images/thank-2.png'
-        // },
         {
           pagePath: 'pages/thanks/thanks',
           text: '致谢',
