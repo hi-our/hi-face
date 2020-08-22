0978ee50c6c733b0c515c0b94e436f9810b49d91
shenghanqin <shenghanqin@163.com>
2020-08-22 20:32:50 +0800

    perf: 更新代码效果

--- cloud/functions/open-api/index.js
+++ cloud/functions/open-api/index.js
@@ -1,5 +1,4 @@
 // 云函数入口文件
-
 const cloud = require('wx-server-sdk')
 
 // 初始化云函数
@@ -7,68 +6,14 @@ cloud.init({
   // API 调用都保持和云函数当前所在环境一致
   env: cloud.DYNAMIC_CURRENT_ENV
 })
-/**
- * 
- * 将 arrayBuffer 转为 buffer 
- * 
-*/
-function toBuffer(ab) {
-
-  let buf = Buffer.from(ab)
-  return buf;
-}
-// 云函数入口函数
-exports.main = async (event, context) => {
-  console.log(event)
-  switch (event.action) {
-    case 'imgSecCheck': {
-      return imgSecCheck(event)
-    }
-    case 'createQRCode': {
-      return createQRCode(event)
-    }
-    case 'createMiniCode': {
-      return createMiniCode(event)
-    }
-    default: {
-      return
-    }
-  }
-}
 
 /**
-* 函数imgSecCheck
-* 参数 event:{
-*    file //上传的文件
-*  }
-*   
-*/
-
-async function imgSecCheck(event) {
-  let { file } = event
-  console.log('file :', file);
-  const result = await cloud.openapi.security.imgSecCheck({
-    media: {
-      contentType: 'image/png',
-      value: toBuffer(file)
-    }
-  })
-  return {
-    data: result,
-    time: new Date(),
-    status: result.errCode || 0,
-    message: result.errMsg || ''
-  }
-}
-/**
-* 函数imgSecCheck
+* createQRCode 二维码
 * 参数 event:{
 *    path //路径
 *    width // 宽度
 *  }
-*   
 */
-
 async function createQRCode(event) {
   let { path = '', width = 300 } = event
 
@@ -99,14 +44,12 @@ async function createQRCode(event) {
   }
 }
 /**
-* 函数imgSecCheck
+* createMiniCode 小程序码
 * 参数 event:{
 *    path //路径
 *    width // 宽度
 *  }
-*   
 */
-
 async function createMiniCode(event) {
   let { path = '', width = 300 } = event
 
@@ -137,3 +80,16 @@ async function createMiniCode(event) {
   }
 }
 
+// 云函数入口函数
+exports.main = async (event, context) => {
+  switch (event.action) {
+    case 'createQRCode': {
+      return createQRCode(event)
+    }
+    case 'createMiniCode': {
+      return createMiniCode(event)
+    }
+    default: {
+    }
+  }
+}
--- taro/src/pages/avatar-poster/avatar-poster.js
+++ taro/src/pages/avatar-poster/avatar-poster.js
@@ -43,7 +43,7 @@ class AvatarPoster extends Component {
 
   componentDidMount() {
     this.loadData()
-    // this.onCreateQrcode()
+    this.onCreateQrcode()
   }
 
   onShareAppMessage() {
