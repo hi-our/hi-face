a035a4920785f57206dec49d3e35aabcd6bad143
shenghanqin <shenghanqin@163.com>
2020-09-25 13:29:36 +0800

    feat: 部署test效果

--- taro/config/index.js
+++ taro/config/index.js
@@ -1,7 +1,7 @@
 const alias = require('./alias')
 const loadEnv = require('./load-env')
 
-loadEnv(process.env.SERVER_ENV === 'dev' ? '.env.dev' : '.env')
+loadEnv(process.env.SERVER_ENV != 'prod' ? '.env.' + process.env.SERVER_ENV : '.env')
 // 兼容web端和小程序端
 const dpr = process.env.TARO_ENV === 'h5' ? 1 : 2
 
